e530354f499714886f8448e1d337f64fc1f282d3
Mahmoud_nfz <mahmoudhnefzi@gmail.com>
2023-04-05 03:56:22 +0100

    changed simulation behavior and added speed control

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const NCOLS = 10, NROWS = 10;
+const speedSlider = document.getElementById("speed-slider");
+const speedValue = document.querySelector(".slider-value");
+let SIMULATION_SPEED = speedSlider.value;
+
+speedSlider.addEventListener("input", function() {
+  speedValue.innerHTML = this.value;
+});
+
+speedSlider.addEventListener("change", function() {
+    SIMULATION_SPEED = this.value;
+});
+
+// add event listener to reset-button
+document.querySelector('#reset-button').addEventListener('click', ()=>{
+    window.location.reload();
+});
 
 const gameBoard = document.querySelector('.game-board');
-// add the to game board
-// grid-template-columns: repeat(10, 1fr);
-// grid-template-rows: repeat(10, 1fr);
 gameBoard.style.gridTemplateColumns = `repeat(${NCOLS}, 1fr)`;
 gameBoard.style.gridTemplateRows = `repeat(${NROWS}, 1fr)`;
 
@@ -84,40 +97,49 @@ console.log(obstacle);
 const gamma = 0.8;
 async function simulate(){
     console.log("Simulation began");
+    x = parseInt(startRow);
+    y = parseInt(startCol);
     while(true){
         let prevValues = values.slice();
-        for(let i=0; i<NROWS; i++){
-            for(let j=0; j<NCOLS; j++){
-                if(obstacle[i][j] || rewards[i][j]){
-                    continue;
-                }
-                x = parseInt(i);
-                y = parseInt(j);
+        // change current cell color
+        document.querySelector(`#cell-${x}-${y}`).classList.add('cell-current');
+        // sleep for 1 second
+        await new Promise(resolve => setTimeout(resolve, 300/SIMULATION_SPEED));
+        // for(let i=0; i<NROWS; i++){
+        //     for(let j=0; j<NCOLS; j++){
+                // if(obstacle[i][j] || rewards[i][j]){
+                //     continue;
+                // }
+                // x = parseInt(i);
+                // y = parseInt(j);
                 console.log("currently at ", x, y);
                 let currValue = -9999999;
-                // let possibleMoves = [];
-                // treat everything as float
+                let possibleMoves = [];
                 if(x && !obstacle[x-1][y]){
                     currValue = Math.max(currValue, parseFloat(rewards[x-1][y]) + gamma * parseFloat(prevValues[x-1][y]));
-                    // possibleMoves.push([x-1, y]);
+                    if(!rewards[x-1][y])
+                        possibleMoves.push([x-1, y]);
                     // console.log("currValue = ", currValue);
                 }
                 if(x<NROWS-1 && obstacle[x+1] && !obstacle[x+1][y]){
                     // currValue = Math.max(currValue, rewards[x+1][y] + gamma * prevValues[x+1][y]);
                     currValue = Math.max(currValue, parseFloat(rewards[x+1][y]) + gamma * parseFloat(prevValues[x+1][y]));
-                    // possibleMoves.push([x+1, y]);
+                    if(!rewards[x+1][y])
+                        possibleMoves.push([x+1, y]);
                     // console.log("currValue = ", currValue);
                 }
                 if(y && !obstacle[x][y-1]){
                     // currValue = Math.max(currValue, rewards[x][y-1] + gamma * prevValues[x][y-1]);
                     currValue = Math.max(currValue, parseFloat(rewards[x][y-1]) + gamma * parseFloat(prevValues[x][y-1]));
-                    // possibleMoves.push([x, y-1]);
+                    if(!rewards[x][y-1])
+                        possibleMoves.push([x, y-1]);
                     // console.log("currValue = ", currValue);
                 }
                 if(y<NCOLS-1 && !obstacle[x][y+1]){
                     // currValue = Math.max(currValue, rewards[x][y+1] + gamma * prevValues[x][y+1]);
                     currValue = Math.max(currValue, parseFloat(rewards[x][y+1]) + gamma * parseFloat(prevValues[x][y+1]));
-                    // possibleMoves.push([x, y+1]);
+                    if(!rewards[x][y+1])
+                        possibleMoves.push([x, y+1]);
                     // console.log("currValue = ", currValue);
                 }
                 values[x][y] = parseFloat(currValue);
@@ -126,6 +148,8 @@ async function simulate(){
                 document.querySelector(`#cell-${x}-${y}`).innerHTML = currValue.toFixed(2);
                 document.querySelector(`#cell-${x}-${y}`).style.fontSize = '14px';
                 // depedning upon the value of the cell, change the color of the cell to warm / moderatly warm / cold / moderatly cold / neutral
+                // remove this         document.querySelector(`#cell-${x}-${y}`).style.backgroundColor = '#8b43cf';
+                document.querySelector(`#cell-${x}-${y}`).classList.remove('cell-current');
                 if(currValue > 0.5){
                     document.querySelector(`#cell-${x}-${y}`).classList.remove('cell-2');
                     document.querySelector(`#cell-${x}-${y}`).classList.add('cell-warm');
@@ -145,14 +169,15 @@ async function simulate(){
                 else{
                     document.querySelector(`#cell-${x}-${y}`).classList.add('cell-2');
                 }
+
                 // randomly go to next cell
-                // let nextMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-                // x = nextMove[0];
-                // y = nextMove[1];
-            }
-        }
+                let nextMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+                x = nextMove[0];
+                y = nextMove[1];
+        //     }
+        // }
         console.log(values);
         // sleep for 1 second
-        await new Promise(resolve => setTimeout(resolve, 500));
+        // await new Promise(resolve => setTimeout(resolve, 500));
     }
 }
